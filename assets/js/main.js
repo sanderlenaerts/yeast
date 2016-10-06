815ee0e483f7c560e8175d921b9b39dcabd488b1
sanderlenaerts <sander.lenaerts@gmail.com>
2016-10-06 17:26:18 +0200

    Fixed forgotten bracket

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,21 +15,11 @@ $(document).ready(function () {
     $('#navigation').toggleClass('reveal');
   });
 
-
-
-
-  $('body').keyup(function(event) {
-    event.preventDefault();
-
-    var code = event.keyCode || event.which;
-
    $(window).on('resize', function(){
       var win = $(this); //this = window
       if (win.width() >= 540) {
         $('#main-content').removeClass('reveal');
         $('#navigation').removeClass('reveal');
       }
-    });
-
   });
 });
